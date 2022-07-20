a6c37fa9604827d488196808c5c3e5cd03d4a846
solmonteiro <sole7mares@gmail.com>
2022-07-20 08:26:46 -0300

    Criando dropdown menu1

--- app/src/main/java/com/example/listadetarefas2/MainActivity.kt
+++ app/src/main/java/com/example/listadetarefas2/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.listadetarefas2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list: MutableList<String>=ArrayList()
+        list.add("PROGRAMADOR ANDROID JÚNIOR")
+        list.add("PROFESSOR UNIVERSITÁRIO")
+
+val adapter:ArrayAdapter<String!> = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+     spnTest.adapter=adapter
     }
+
+
 }
