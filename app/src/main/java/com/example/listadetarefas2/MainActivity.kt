f8ba2f4468992fdedd20a44f200a0fd1d7e14e38
solmonteiro <sole7mares@gmail.com>
2022-07-21 10:42:35 -0300

    Tela inclusão_criação

--- app/src/main/java/com/example/listadetarefas2/MainActivity.kt
+++ app/src/main/java/com/example/listadetarefas2/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.example.listadetarefas2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list: MutableList<String>=ArrayList()
-        list.add("PROGRAMADOR ANDROID JÚNIOR")
-        list.add("PROFESSOR UNIVERSITÁRIO")
+val btnIncluirTarefa:Button =findViewById(R.id.btnIncluirTarefa)
 
-val adapter:ArrayAdapter<String!> = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
-     spnTest.adapter=adapter
+btnIncluirTarefa.setOnClickListener {
+    val intent=Intent(this,tela_inclusao_activity::class.java)
+    startActivity(intent)
+}
+
     }
 
 
---
+++ app/src/main/java/com/example/listadetarefas2/tela_inclusao_activity.kt
@@ -0,0 +1,11 @@
+package com.example.listadetarefas2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela_inclusao_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inclusao)
+    }
+}
