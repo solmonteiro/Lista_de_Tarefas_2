a49c6ab6dee5ff7f34857155d09674bc55bf7ca3
solmonteiro <sole7mares@gmail.com>
2022-08-03 10:43:51 -0300

    alteração layout item_recycler.xml

--- app/src/main/java/com/example/listadetarefas2/Tela_inclusao_activity.kt
+++ app/src/main/java/com/example/listadetarefas2/Tela_inclusao_activity.kt
@@ -14,6 +14,7 @@ import java.util.*
 class Tela_inclusao_activity : MainActivity() {
 
 
+
     private lateinit var et_areaVida: EditText
     private lateinit var et_objetivo: EditText
     private lateinit var et_tarefa: EditText
